ddc366cf77b5fa6f51d8668d5bbd65f7608fcfbb
Cristóbal Tello <cristobal.tello@outlook.com>
2019-06-06 14:52:35 +0200

    Add solution and projects

---
+++ App.Data/SamuraiContext.cs
@@ -0,0 +1,17 @@
+﻿using App.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace App.Data
+{
+    public class SamuraiContext : DbContext
+    {
+        public DbSet<Samurai> Samurais { get; set; }
+        public DbSet<Quote> Quotes { get; set; }
+        public DbSet<Battle> Battles { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server = (localdb)\\mssqllocaldb; Database=SamuraiAppData; Trusted_Connection=True");
+        }
+    }
+}
