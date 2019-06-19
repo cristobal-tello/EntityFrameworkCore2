ed3ca5a9c973f951d0aa95cf17a04f9f30d29b9e
Cristóbal Tello <cristobal.tello@outlook.com>
2019-06-19 15:48:34 +0200

    Batching Commands When Saving

--- App.SomeUI/Program.cs
+++ App.SomeUI/Program.cs
@@ -9,9 +9,39 @@ namespace App.SomeUI
         static void Main(string[] args)
         {
             InsertSamurai();
-
+            InsertMultipleSamurais();
+            InsertMultipleDifferentObjets();
             Console.WriteLine("Finsihed!!!!!");
             Console.ReadKey();
+        }
+
+        private static void InsertMultipleSamurais()
+        {
+            var samurai1 = new Samurai() { Name = "Charles" };
+            var samurai2 = new Samurai() { Name = "Tom" };
+
+            using (var context = new SamuraiContext())
+            {
+                context.Samurais.AddRange(samurai1, samurai2);
+                context.SaveChanges();
+            }
+        }
+
+        private static void InsertMultipleDifferentObjets()
+        {
+            var samurai = new Samurai() { Name = "Robert" };
+            var battle = new Battle()
+            {
+                Name = "Any battle",
+                StartDate = new DateTime(2000, 11, 29),
+                EndDate = new DateTime(2001, 12, 24)
+            };
+
+            using (var context = new SamuraiContext())
+            {
+                context.AddRange(samurai, battle);
+                context.SaveChanges();
+            }
         }
 
         private static void InsertSamurai()
