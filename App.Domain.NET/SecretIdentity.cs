69e8c77f27abea588ffbdbf33f191ad0d26a206f
Cristóbal Tello <cristobal.tello@outlook.com>
2019-07-09 18:07:47 +0200

    Remove unused .NET projects

--- App.Domain.NET/SecretIdentity.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace App.Domain
-{
-    public class SecretIdentity : ClientChangeTracker
-    {
-        public int Id { get; set; }
-        public string RealName { get; set; }
-        public int SamuraiId { get; set; }
-    }
-}
